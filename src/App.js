7c5b8a41001032a0c36a0a0ce4802e737c235b83
Muhammad Ahad <muhammadahad9615@gmail.com>
2020-07-06 02:08:50 +0500

    added delete todo function

--- src/App.js
+++ src/App.js
@@ -32,12 +32,19 @@ class App extends Component {
         return todo;
       })
     });
-  };
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  }
   
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
   }
