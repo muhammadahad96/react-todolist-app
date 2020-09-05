0fd73ba47ff44089ac4145863533128a32ae53e5
muhammadahad96 <muhammadahad9615@gmail.com>
2020-09-05 22:01:37 +0500

    Updates

---
+++ precache-manifest.22730040ef58f4731732d9e5b1bfa8b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9a79bdf4a263ada6794c15ffd670d9",
+    "url": "/react-todolist-app/index.html"
+  },
+  {
+    "revision": "181832056688384890a6",
+    "url": "/react-todolist-app/static/css/main.652b4183.chunk.css"
+  },
+  {
+    "revision": "c353008be5b005b5503d",
+    "url": "/react-todolist-app/static/js/2.f4743abc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todolist-app/static/js/2.f4743abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181832056688384890a6",
+    "url": "/react-todolist-app/static/js/main.82cca650.chunk.js"
+  },
+  {
+    "revision": "e8c9a5ebe61880f78c56",
+    "url": "/react-todolist-app/static/js/runtime-main.bddf3ca0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-todolist-app/precache-manifest.22730040ef58f4731732d9e5b1bfa8b8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todolist-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
